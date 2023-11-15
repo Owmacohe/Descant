b2e3b43803d21ceed028b9dbc8fcac8ab7df7d6e
Owmacohe <44061383+Owmacohe@users.noreply.github.com>
2023-11-15 10:44:25 -0500

    [refactor] Cleared the repo for a sec...

--- Packages/Descant-0.1.1/Utilities/DescantUtilities.cs
+++
@@ -1,7 +0,0 @@
-namespace DescantUtilities
-{
-    /// <summary>
-    /// Enum to list all of the types of DescantNodes
-    /// </summary>
-    public enum DescantNodeType { Choice, Response, Start, End, Any }
-}
